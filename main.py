8d7b3495e12da3d6daf63fc08f1266e225c288b4
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-09-29 16:10:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+#assignment operator
+a = 10
+a += 5
+print(a)
+
+#-
+a = 10
+a -= 5
+print(a)
+
+#*
+a = 10
+a *= 5
+print(a)
+
+#/
+a = 10
+a /= 5
+print(a)
+
+#%
+a = 10
+a %= 5
+print(a)
+
+#//
+a = 10
+a //= 5
+print(a)
+
+#and operator
+a = 5
+b = 6
+c = 8
+print( a>b and a>c )
+print( a<b and a<c )
+
+#or operator
+
+a = 5
+b = 6
+c = 8
+print( a>b or a>c )
+print( a<b or a<c )
+
+#not operator
+
+a = 5
+b = 6
+c = 8
+print((not a>b or a>c ))
+print((not a<b or a<c ))
+
+#==
+a = 45
+b = 50
+print(a==b)
+
+#< =
+
+a = 45
+b = 50
+print(a<=b)
+
+#>=
+
+a = 45
+b = 50
+print(a>=b)
+
+#/
+a = 45
+b = 50
+print(a/b)
+
+#<
+
+a = 45
+b = 50
+print(a<b)
+
+
+#>
+
+a = 45
+b = 50
+print(a>b)
+
+#/=
+
+a = 45
+b = 50
+print(a!=b)
+
+#bitwise and operator
+
+a = 5
+b = 9
+print( a&b )
+
+#bitwise or operator
+a = 8
+b = 5
+print( a|b )
+print( 25/5 )
+
+
+
+
